6cf8b789d92241024cf369b4db5c79166f572095
cyberenjeru <viniciusbernardesxcx@gmail.com>
2023-10-10 21:56:00 -0300

    utilizando o metodo forEach

--- src/Collections/ExerciciosStreamAPI.java
+++ src/Collections/ExerciciosStreamAPI.java
@@ -2,6 +2,7 @@ package Collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ExerciciosStreamAPI {
     public static void main(String[] args) {
@@ -9,5 +10,11 @@ public class ExerciciosStreamAPI {
                 Arrays.asList("1","0","4","1","2","3","9","9","6","5");
 
         System.out.println("Imprima todos os elementos dessa lsita de String:");
+        numerosAleatorios.stream().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
     }
 }
