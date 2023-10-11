68c519f7fd246d7b8b467365f8e5ff860024cf7d
cyberenjeru <viniciusbernardesxcx@gmail.com>
2023-10-10 22:53:31 -0300

    utilizando o metodo removeIf de list

--- src/Collections/ExerciciosStreamAPI.java
+++ src/Collections/ExerciciosStreamAPI.java
@@ -3,6 +3,8 @@ package Collections;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ExerciciosStreamAPI {
@@ -17,9 +19,16 @@ public class ExerciciosStreamAPI {
         numerosAleatorios.stream().limit(5).collect(Collectors.toSet()).forEach(System.out::println);
 
         System.out.println("Transforme esta lista de String em uma lista de números inteiros");
-        List<Integer> numerosAleatorios1 = numerosAleatorios.stream().map(Integer::parseInt).toList();
+        List<Integer> numerosAleatorios1 = new java.util.ArrayList<>(numerosAleatorios.stream().map(Integer::parseInt).toList());
 
         System.out.println("Pegue os números pares e maiores que 2 e coleque em uma lista");
-        numerosAleatorios.stream().map(Integer::parseInt).filter(i -> i % 2 == 0 && i > 2).collect(Collectors.toList()).forEach(System.out::println);
+        numerosAleatorios.stream().map(Integer::parseInt).filter(i -> i % 2 == 0 && i > 2).toList().forEach(System.out::println);
+
+        System.out.println("Mostre a média dos números");
+        numerosAleatorios.stream().mapToInt(Integer::parseInt).average().ifPresent(System.out::println);
+
+        System.out.println("Remova os números ímpares");
+        numerosAleatorios1.removeIf(i -> (i % 2 != 0));
+        System.out.println(numerosAleatorios1);
     }
 }
