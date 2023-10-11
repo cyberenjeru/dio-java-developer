cec1b5e92de65f3890eabd18c58b2d97b0fd7411
cyberenjeru <viniciusbernardesxcx@gmail.com>
2023-10-10 21:54:17 -0300

    aula stream api

---
+++ src/Collections/ExerciciosStreamAPI.java
@@ -0,0 +1,13 @@
+package Collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExerciciosStreamAPI {
+    public static void main(String[] args) {
+        List<String> numerosAleatorios =
+                Arrays.asList("1","0","4","1","2","3","9","9","6","5");
+
+        System.out.println("Imprima todos os elementos dessa lsita de String:");
+    }
+}
